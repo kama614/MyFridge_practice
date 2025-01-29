3950e6f759058bbe84b997b9a350e8a2cee2b330
kamada <kamada@example.com>
2025-01-29 14:23:36 +0900

    ループ処理を追加
    ５番を選択するまでループする

--- src/myfridge/UserInterface.java
+++ src/myfridge/UserInterface.java
@@ -7,42 +7,47 @@ public class UserInterface {
 
 	// ユーザーの選択した番号に応じて、各画面を呼び出す
 	public void run() {
-		// ユーザーに操作を選択させる
-		System.out.println("何をしますか？");
-		System.out.println("1:冷蔵庫の中身を見る 2:賞味期限切れ一覧 3:追加 4:削除 5:終了");
 
-		// 入力値の取得
-		var sc = new Scanner(System.in);
-		String strChoice = sc.nextLine();
+		boolean loopFlag = true;
+		while (loopFlag) {
+			// ユーザーに操作を選択させる
+			System.out.println("何をしますか？");
+			System.out.println("1:冷蔵庫の中身を見る 2:賞味期限切れ一覧 3:追加 4:削除 5:終了");
 
-		// 入力された番号を整数に変換 String ⇒ int
-		int choice;
-		try {
-			choice = Integer.parseInt(strChoice);
-		} catch (NumberFormatException e) {
-			//変換できなかった場合、「5：終了」とする
-			choice = 5;
+			// 入力値の取得
+			var sc = new Scanner(System.in);
+			String strChoice = sc.nextLine();
+
+			// 入力された番号を整数に変換 String ⇒ int
+			int choice;
+			try {
+				choice = Integer.parseInt(strChoice);
+			} catch (NumberFormatException e) {
+				//変換できなかった場合、「5：終了」とする
+				choice = 5;
+			}
+
+			// 番号に応じて画面を呼び出す
+			switch (choice) {
+			case 1:
+				showItems();
+				break;
+			case 2:
+				showExpiredItems();
+				break;
+			case 3:
+				addItem();
+				break;
+			case 4:
+				deleteItem();
+				break;
+			default:
+				System.out.println("システムを終了しました");
+				sc.close();
+				loopFlag = false;
+			}
+
 		}
-
-		// 番号に応じて画面を呼び出す
-		switch (choice) {
-		case 1:
-			showItems();
-			break;
-		case 2:
-			showExpiredItems();
-			break;
-		case 3:
-			addItem();
-			break;
-		case 4:
-			deleteItem();
-			break;
-		default:
-			System.out.println("終了");
-
-		}
-		sc.close();
 
 	}
 
