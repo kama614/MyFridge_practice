d0ff3994d1f7a7f093f67c014959152158d462dc
kamada <kamada@example.com>
2025-01-29 15:08:06 +0900

    テスト用クラスの作成

---
+++ src/test/ListTextConverterTest.java
@@ -0,0 +1,19 @@
+package test;
+// ListTextConverterのテスト用のクラス
+
+import myfridge.ListTextConverter;
+
+public class ListTextConverterTest {
+
+	private static ListTextConverter converter = new ListTextConverter();
+
+	public static void main(String[] args) {
+		testRead();
+	}
+
+	// テキストファイルの読み込みテスト
+	private static void testRead() {
+		converter.read();
+	}
+
+}
