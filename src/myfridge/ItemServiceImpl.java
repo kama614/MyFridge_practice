b731dac7b1ede1289e2db0c2b5bc073795142396
kamada <kamada@example.com>
2025-01-29 15:54:51 +0900

    実装クラスのItemServiceImpl作成

---
+++ src/myfridge/ItemServiceImpl.java
@@ -0,0 +1,34 @@
+package myfridge;
+// アイテムを操作するためのクラス（ItemServiceインターフェースの実装）
+
+import java.util.List;
+
+public class ItemServiceImpl implements ItemService {
+
+	//全アイテムを取得
+	
+	@Override
+	public List<Item> getItemList() {
+		var converter = new ListTextConverter();
+		return converter.read();
+	}
+	
+	@Override
+	public List<Item> getExpiredList() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	
+	@Override
+	public void addItem(Item item) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	
+	@Override
+	public void deleteItem(int index) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
