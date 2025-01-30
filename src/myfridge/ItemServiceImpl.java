7fb315e88c7429c6fe5ec9782be5fa99aee90667
kamada <kamada@example.com>
2025-01-30 15:54:46 +0900

    ItemServiceImplの編集
    賞味期限アイテムを絞り込むためのロジックを検討。ListTextConverterオブジェクトは、全メソッドで利用するので、コンストラクタ内で生成

--- src/myfridge/ItemServiceImpl.java
+++ src/myfridge/ItemServiceImpl.java
@@ -1,34 +1,51 @@
 package myfridge;
 // アイテムを操作するためのクラス（ItemServiceインターフェースの実装）
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ItemServiceImpl implements ItemService {
 
+	private ListTextConverter converter;
+
+	public ItemServiceImpl() {
+		converter = new ListTextConverter();
+	}
+
 	//全アイテムを取得
-	
+
 	@Override
 	public List<Item> getItemList() {
 		var converter = new ListTextConverter();
 		return converter.read();
 	}
-	
+
 	@Override
 	public List<Item> getExpiredList() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+
+		// 賞味期限切れアイテムを格納するためのList
+		List<Item> expiredList = new ArrayList<>();
+
+		// 全アイテムのList
+		List<Item> itemList = converter.read();
+		
+		// ①各アイテムを拡張for文で取り出し
+		for(Item item:itemList) {
+			// ②賞味期限切れの場合、expiredListに追加
+		}
+		return expiredList;
 	}
-	
+
 	@Override
 	public void addItem(Item item) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
-	
+
 	@Override
 	public void deleteItem(int index) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 }
